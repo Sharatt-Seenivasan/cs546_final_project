e5f1c513bc2ec669dc3b29a83bea2d938a5b90ed
HyperPigeon <49329786+Sharatt-Seenivasan@users.noreply.github.com>
2023-04-08 21:49:37 -0400

    Initial route set-up Fixes #7

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+import birdRoutes from './birds.js';
+import userRoutes from './users.js';
+import path from 'path';
+
+const constructorMethod = (app) => {
+
+
+}
+
+export default constructorMethod;
