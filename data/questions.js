515cbfeefee9171ab0d590d484718549d7602d60
sam <saminusama@gmail.com>
2023-04-27 19:13:49 -0400

    update data/questions.js: appending fields: "_id" and "difficulty"

--- data/questions.js
+++ data/questions.js
@@ -71,11 +71,13 @@ const getQuestions4User = async (
     while (options.length < numberOfOptions && unseenBirds.length > 0) {
       rdmUnseenBirdIdx = Math.floor(Math.random() * unseenBirds.length);
       const theBird = unseenBirds[rdmUnseenBirdIdx];
-      const birdName = theBird.names[0]; // take 1st name by default
-      options.push(birdName);
+      const birdNames = theBird.names[0]; // take 1st name by default
+      options.push(birdNames);
       if (options.length === rdmAnswerIdx + 1) {
-        q["answer"] = birdName;
+        q["answer"] = birdNames;
         q["image"] = theBird.url;
+        q['_id'] = theBird._id;
+        q['difficulty'] = theBird.difficulty;
         unseenBirds.splice(rdmUnseenBirdIdx, 1);
       }
     }
@@ -144,11 +146,13 @@ const getQuestions4Guest = async ({
     while (options.length < numberOfOptions && allBirds.length > 0) {
       rdmBirdIdx = Math.floor(Math.random() * allBirds.length);
       const theBird = allBirds[rdmBirdIdx];
-      const birdName = theBird.names[0]; // take 1st name by default
-      options.push(birdName);
+      const birdNames = theBird.names[0]; // take 1st name by default
+      options.push(birdNames);
       if (options.length === rdmAnswerIdx + 1) {
-        q["answer"] = birdName;
+        q["answer"] = birdNames;
         q["image"] = theBird.url;
+        q['_id'] = theBird._id;
+        q['difficulty'] = theBird.difficulty;
         allBirds.splice(rdmBirdIdx, 1);
       }
     }
