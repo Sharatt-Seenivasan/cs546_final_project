756fcf22f9b5abf9caf49a69c1c363a5db49482e
Sharatt-Seenivasan <49329786+Sharatt-Seenivasan@users.noreply.github.com>
2023-04-29 22:48:55 -0400

    Added middleware that redirects users from gameplay and gameresults pages when necessary

--- middleware.js
+++ middleware.js
@@ -1,25 +1,38 @@
-export function saveLastPage(req,res,next){
+export async function saveLastPage(req,res,next){
     req.session.lastPage = req.originalUrl;
     next()
 }
 
-export function loginRedirect(req, res, next){
+export async function loginRedirect(req, res, next){
     if(req.session.user) {
         return res.redirect('/');
     }
     next();
 }
 
-export function registerRedirect(req, res, next){
+export async function registerRedirect(req, res, next){
     if(req.session.user) {
         return res.redirect('/');
     }
     next();
 }
 
-export function logoutRedirect(req, res, next){
+export async function logoutRedirect(req, res, next){
     if(!req.session.user) {
         return res.redirect('/users/login');
     }
     next();
+}
+
+
+export async function gameplayRedirect(req, res, next){
+    if(req.session.lastPage !== '/users/gamestart'){
+        return res.redirect('/');
+    }
+}
+
+export async function gameResultRedirect(req, res, next){
+    if(req.session.lastPage !== '/users/gameplay'){
+        return res.redirect('/');
+    }
 }
